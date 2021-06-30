4019d77c5187a4baa13939d65882bb982d555835
Philip Rodrigues <phiroict@gmail.com>
2021-06-30 17:46:40 +1200

    Added pull-secrets and location to the terraform output.

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,5 +3,5 @@ output "tls_private_key" {
 }
 
 output "aro_create_command" {
-  value = "az aro create --resource-group ${azurerm_resource_group.myterraformgroup.name} --name aro_cluster --vnet ${azurerm_virtual_network.myterraformnetwork.name} --master-subnet ${azurerm_subnet.myControlPlaneSubnet.name} --worker-subnet ${azurerm_subnet.myWorkerSubnet.name} --apiserver-visibility Private --ingress-visibility Private --worker-vm-size Standard_D4as_v4"
+  value = "az aro create --resource-group ${azurerm_resource_group.myterraformgroup.name} --name aro_cluster --domain ${azurerm_private_dns_zone.privatedomain.name} --vnet ${azurerm_virtual_network.myterraformnetwork.name} --master-subnet ${azurerm_subnet.myControlPlaneSubnet.name} --worker-subnet ${azurerm_subnet.myWorkerSubnet.name} --apiserver-visibility Private --ingress-visibility Private --worker-vm-size Standard_D4as_v4 --location ${azurerm_resource_group.myterraformgroup.location} --pull-secret @pull-secret.txt"
 }
